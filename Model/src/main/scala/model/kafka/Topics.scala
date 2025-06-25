a826a73855563b0740dbfe15cbb04fdd8418892d
bastian <ba121rom@htwg-konstanz.de>
2025-06-25 12:23:01 +0200

    wip: kafka topics

---
+++ Model/src/main/scala/model/kafka/Topics.scala
@@ -0,0 +1,13 @@
+package model.kafka
+
+
+object Topics {
+  
+  object GameTopic {
+    object UpdateGameKey {
+      override val toString = "UpdateGameKey"
+    }
+    
+    override val toString = "GameTopic"
+  }
+}
